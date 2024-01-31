38c7089f6cb900989e1fa895d5f60125466dc9df
it_is_ik <154598688+1t1s1k@users.noreply.github.com>
2024-01-31 22:19:06 +0600

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ game_icon = pygame.image.load('C:\gameguy\gameicon.png')
 pygame.display.set_icon(game_icon)
 
 #Штука для ФПС
+FPS = 60
 clock = pygame.time.Clock()
 
 #Фоновая музыка игры
@@ -20,14 +21,30 @@ pygame.mixer.music.load("C:\gameguy\guy\song1.mp3")
 ground = pygame.image.load('C:\gameguy\ground.jpg')
 pygame.mixer.music.play(-1)
 
+#Положение обьекта на экране
+x = 1
+y = 1
+#Скорость движения
+speed = 1
 
 while True:
+    
     #Вывод картинки
     screen.blit(ground,(0,0))
+    
     #Обновленее Экрана
     pygame.display.update()
-    #Цикл для того чтобы коректно открывалось окно
+    
+    # Цикл для того чтобы коректно открывалось окно
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+            
+        #Выбор Варианта по нажатию на Клавишу (НЕ ЗАКОНЧИЛ)
+        elif event.type == pygame.KEYDOWN:
+            screen.blit()
+        if event.key == pygame.KEYUP:
+
+
+
